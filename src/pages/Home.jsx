2665442ebfeea1982f12c6d44c209b9660e51456
vatsya <vatsyaaggarwal62@gmail.com>
2026-05-06 19:06:31 +0530

    fetched api, made home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import ProductCard from '../components/ProductCard';
+import './Home.css';
 
 function Home(){
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      })
+      .catch(err => {
+        console.error("Error fetching products:", err);
+      });
+    }, []);
     return(
-        <>
-        <ProductCard/>
-        </>
+        <div className="home-container">
+            <h2>Featured Products</h2>
+            <div className="product-grid">
+                {products.map(product => (
+                <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
     )
 }
-
-
-
-
 
 
 export default Home;
